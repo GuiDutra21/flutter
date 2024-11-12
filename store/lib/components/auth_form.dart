e43e3995d8a2a551e09122ec0b34c7fe66b8b31c
GuiDutra21 <221021984@aluno.unb.br>
2024-11-12 11:30:49 -0300

    termina a estetica da pagina de login/registro

--- store/lib/components/auth_form.dart
+++ store/lib/components/auth_form.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-enum AuthMode { login, sigUp }
+enum AuthMode { login, signUp }
 
 class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
@@ -12,11 +12,53 @@ class AuthForm extends StatefulWidget {
 void _submit() {}
 
 class _AuthFormState extends State<AuthForm> {
+  var _passWordController = TextEditingController();
+  var _formKey = GlobalKey<FormState>();
+
+  bool _isLoading = false;
+
+  AuthMode _authMode = AuthMode.login;
+
+  Map<String, String> _authData = {'Email': '', 'Senha': ''};
+
+  bool isLogin() => _authMode == AuthMode.login ? true : false;
+  bool isSignUp() => _authMode == AuthMode.signUp ? true : false;
+
+  void switchAuthMode() {
+    setState(() {
+      isLogin() ? _authMode = AuthMode.signUp : _authMode = AuthMode.login;
+    });
+  }
+
+  void _submit() {
+
+    final isValid = _formKey.currentState!.validate();
+
+    if(!isValid)
+      return;
+    
+    _formKey.currentState!.save(); // Salva os dados passado no textFormField no atributo save
+
+    if( isLogin())
+    {
+      // Login
+    }
+    else
+    {
+      // Resgristrar
+    }
+  
+    setState(() =>
+      _isLoading = true
+    );
+    setState(() =>
+      _isLoading = false
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
-
-    AuthMode authMode = AuthMode.login;
 
     return Card(
       margin: const EdgeInsets.only(top: 50),
@@ -24,45 +66,76 @@ class _AuthFormState extends State<AuthForm> {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       child: Container(
         padding: const EdgeInsets.all(16),
-        height: authMode == AuthMode.login ? 270 : 320,
+        height: isLogin() ? 330 : 400,
         width: screenSize.width * 0.8,
         child: Form(
+          key: _formKey, // Para savar as informações
           child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               TextFormField(
                 decoration: const InputDecoration(labelText: "E-mail"),
                 keyboardType: TextInputType.emailAddress,
+                onSaved: (email) => _authData['Email'] = email ?? '',
+                validator: (_email) {
+                  final email = _email ?? '';
+                  if (email.isEmpty || !email.contains('@')) {
+                    return 'Informe um e-mail válido';
+                  }
+                  return null;
+                },
               ),
-
               TextFormField(
                 decoration: const InputDecoration(labelText: "Senha"),
                 keyboardType: TextInputType.emailAddress,
                 obscureText: true,
+                onSaved: (passWord) => _authData['Senha'] = passWord ?? '',
+                controller: _passWordController,
+                validator: (_passWord) {
+                  final passWord = _passWord ?? '';
+                  if (passWord.isEmpty || passWord.length < 3) {
+                    return 'Informe uma senha com mais de 3 caracteres';
+                  }
+                  return null;
+                },
               ),
-
-              if (authMode == AuthMode.sigUp)
+              if (isSignUp())
                 TextFormField(
                   decoration:
                       const InputDecoration(labelText: "Confirmar senha"),
                   keyboardType: TextInputType.emailAddress,
                   obscureText: true,
+                  validator: (_confirmed) {
+                    final confirmed = _confirmed ?? '';
+                    if (confirmed != _passWordController.text) {
+                      return 'As senhas informadas não coincidem !';
+                    }
+                    return null;
+                  },
                 ),
               const Spacer(flex: 3),
-              InkWell(
-                splashColor: Colors.amber,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    padding: const EdgeInsets.fromLTRB(25,15,25,15)
+               _isLoading ? const CircularProgressIndicator() : Ink(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.blue,
+                ),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(30),
+                  splashColor: Colors.green,
+                  onTap: _submit,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+                    child: Text(
+                      isLogin() ? "ENTRAR" : 'REGISTRAR',
+                      style: const TextStyle(color: Colors.white),
+                    ),
                   ),
-                    onPressed: _submit,
-                    child: authMode == AuthMode.sigUp
-                        ? const Text("REGISTRAR", style: TextStyle(color: Colors.white),)
-                        : const Text("CADASTRAR", style: TextStyle(color: Colors.white),)
                 ),
               ),
-              const Spacer(flex: 1),
+              const Spacer(flex: 3),
+              Text(isLogin() ? 'Não tem uma conta ?' : 'já possui conta ?'),
+              TextButton(
+                  onPressed: () => switchAuthMode(),
+                  child: Text(isLogin() ? 'CRIAR CONTA' : 'IR PARA O LOGIN'))
             ],
           ),
         ),
